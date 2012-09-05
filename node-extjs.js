478aef29d6f420539b04d9045749db3f3768f352
mbains <manjinder.bains@gmail.com>
2012-09-04 17:37:13 -0700

    update to ExtJS 4.1.0

--- node-extjs.js
+++ node-extjs.js
@@ -44,9 +44,9 @@ var fileList = [
 	"/extjs/src/data/IdGenerator.js",
 	"/extjs/src/data/SequentialIdGenerator.js",
 
-	"/extjs/src/data/Association.js",
-	"/extjs/src/data/BelongsToAssociation.js",
-	"/extjs/src/data/HasManyAssociation.js",
+	"/extjs/src/data/association/Association.js",
+	"/extjs/src/data/association/BelongsTo.js",
+	"/extjs/src/data/association/HasMany.js",
 
 	"/extjs/src/ModelManager.js",
 
